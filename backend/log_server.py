4c72e31d5376a9d3c3f6e62b958facd05cc09542
EdwardLeeee <e0900115866@gmail.com>
2024-08-22 08:40:40 +0800

    新Web UI & GET Update

--- backend/log_server.py
+++ backend/log_server.py
@@ -4,135 +4,13 @@ import configparser
 from mysql.connector import Error
 from flask_cors import CORS
 import os
-app = Flask(__name__, static_folder='public')
+# 前端 GET
+app = Flask(__name__, static_folder='public', template_folder='templates')
 CORS(app)
-
-# 讀取配置文件
-config = configparser.ConfigParser()
-config.read('db_config.txt')
-
-db_config = {
-    'user': config.get('DEFAULT', 'user'),
-    'password': config.get('DEFAULT', 'password'),
-    'host': config.get('DEFAULT', 'host'),
-    'database': config.get('DEFAULT', 'database'),
-    #'port': config.get('DEFAULT', 'port')  # 新增這一行
-}
-'''
-# 讀取配置文件
-db_config = {
-    'user': os.getenv('DB_USER'),
-    'password': os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'database': os.getenv('DB_NAME')
-}
-'''
-def check_legal_data(data):
-    errors = []
-    validations = {
-        'HOST_NAME': 32,
-        'HOST_IP': 15,
-        'LEVEL': 5,
-        'SYSTEM_TYPE': 20,
-        'PROCESS_NAME': 64,
-        'CONTENT': 512,
-        'LOG_TIME': 19
-    }
-
-    for field, max_len in validations.items():
-        if len(data.get(field, '')) > max_len:
-            errors.append(f'{field} 超過 {max_len} 個字符')
-
-    if errors:
-        print('Wrong data format')
-
-    return errors
-
-
-#創立會回傳連接而且會在server打印訊息
-def create_connection():
-    try:
-        #musql.connector.connect()是建立連接，裡面是連接的相關資訊
-        connection = mysql.connector.connect(**db_config)
-        if connection.is_connected():
-            print("Connected to MySQL database")
-            return connection
-    # try 失敗之後 印出錯誤訊息
-    except Error as e:#Error 是錯誤的類別
-        print("Error while connecting to MySQL", e)
-        return None
-
-def check_miss(data):
-    # 檢查資料是否殘缺
-    required_fields = ['HOST_NAME', 'HOST_IP', 'SYSTEM_TYPE', 'LEVEL', 'PROCESS_NAME', 'CONTENT', 'LOG_TIME']
-    miss_field = []
-    for field in required_fields:
-        if field not in data:
-            miss_field.append(field)
-    if miss_field :
-        print(f'Missing field: {miss_field}')
-
-    return miss_field
-
-#routing路徑為/log 用HTTP的post
-@app.route('/log', methods=['POST'])
-def log():
-    # data為client打來的JSON格式資料
-    # {"HOST_NAME":"XXXX" , "HOST_IP":"XXXX",.....}
-    data = request.get_json()
-    #檢查是否資料缺失
-    missing_field = check_miss(data)
-    if missing_field:
-        return jsonify({'status': 'error', 'message': f'collector missing field {missing_field}'}), 400
-
-    #檢查數據是否合法
-    data_unlegal = check_legal_data(data)
-    if data_unlegal:
-        return jsonify({'status': 'error', 'message': f'Illegal data  {data_unlegal}'}), 402
-
-    #無資料殘缺
-    try:
-        connection = create_connection()
-        if connection:
-            #創一個游標，用來輸入sql指令
-            cursor = connection.cursor()
-
-            #%s是實際要插入的值,參數化input
-            input_order = """
-            INSERT INTO log_data (HOST_NAME, HOST_IP, SYSTEM_TYPE, LEVEL, PROCESS_NAME, CONTENT, LOG_TIME)
-            VALUES (%s, %s, %s, %s, %s, %s, %s)
-            """
-
-            #執行
-            #data是request進來的JSON，{"HOST_NAME":"XX","HOST_IP":"XX"}
-            cursor.execute(input_order, (
-                data['HOST_NAME'], data['HOST_IP'], data['SYSTEM_TYPE'], data['LEVEL'],
-                data['PROCESS_NAME'], data['CONTENT'], data['LOG_TIME']
-            ))
-
-            #把cursor做的事提交
-            #在執行 INSERT、UPDATE 或 DELETE 操作後，需要調用 commit() 方法來提交這些變更。
-            #如果不調用 commit()，這些變更將不會被保存到資料庫中，並且在連接關閉時將被回滾。
-            connection.commit()
-            cursor.close()
-            connection.close()
-            return jsonify({'status': 'success', 'message': 'Log entry added successfully'}), 201
-
-        #連接失敗，告訴client
-        #原因: (1)sql 帳號密碼錯誤 (2)sql service 沒開
-        else:
-            return jsonify({'status': 'error', 'message': 'Database connection failed'}), 500
-   #非資料庫連接錯誤:logger 的 (1) sql 指令錯誤 (2) python code 寫錯了
-    except Error as e:
-        return jsonify({'status': 'error', 'message': str(e)}), 500
 
 @app.route('/', methods=['GET'])
 def serve_index():
-    return send_from_directory(app.static_folder, 'index.html')
-
-@app.route('/<path:path>')
-def serve_static_file(path):
-    return send_from_directory(app.static_folder, path)
+    return send_from_directory(app.template_folder, 'index.html')
 
 @app.route('/search', methods=['GET'])
 def search_logs():
@@ -170,7 +48,7 @@ def search_logs():
         if connection:
             cursor = connection.cursor(dictionary=True)
             cursor.execute(query, query_params)
-            results = cursor.fetchall()
+            results = cursor.fetchall()#提取所有查詢結果
             cursor.close()
             connection.close()
             return jsonify(results), 200
@@ -179,6 +57,126 @@ def search_logs():
 
     except Error as e:# logger 的 (1) sql 指令錯誤 (2) python code 寫錯了
         return jsonify({'status': 'error', 'message': str(e)}), 500
+
+# 接收collector post
+# 從設定檔讀取配置文件
+config = configparser.ConfigParser()
+config.read('db_config.txt')
+
+db_config = {
+    'user': config.get('DEFAULT', 'user'),
+    'password': config.get('DEFAULT', 'password'),
+    'host': config.get('DEFAULT', 'host'),
+    'database': config.get('DEFAULT', 'database'),
+}
+'''
+#  從環境變數讀取配置文件
+db_config = {
+    'user': os.getenv('DB_USER'),
+    'password': os.getenv('DB_PASSWORD'),
+    'host': os.getenv('DB_HOST'),
+    'database': os.getenv('DB_NAME')
+}
+'''
+def check_legal_data(data):
+    errors = []
+    validations = { # db format 參數
+        'HOST_NAME': 32,
+        'HOST_IP': 15,
+        'LEVEL': 5,
+        'SYSTEM_TYPE': 20,
+        'PROCESS_NAME': 64,
+        'CONTENT': 512,
+        'LOG_TIME': 19
+    }
+
+    for field, max_len in validations.items():
+        if len(data.get(field, '')) > max_len:
+            errors.append(f'{field} 超過 {max_len} 個字符')
+
+    if errors:
+        print('Wrong data format')
+
+    return errors
+
+
+#創立會回傳連接而且會在server打印訊息
+def create_connection():
+    try:
+        # mysql.connector.connect()是建立連接，裡面是連接的相關資訊
+        connection = mysql.connector.connect(**db_config)
+        if connection.is_connected():
+            print("Connected to MySQL database")
+            return connection
+    # try 失敗之後 印出錯誤訊息
+    except Error as e:#Error 是錯誤的類別
+        print("Error while connecting to MySQL", e)
+        return None
+
+def check_miss(data):
+    # 檢查資料是否殘缺
+    required_fields = ['HOST_NAME', 'HOST_IP', 'SYSTEM_TYPE', 'LEVEL', 'PROCESS_NAME', 'CONTENT', 'LOG_TIME']
+    miss_field = []
+    for field in required_fields:
+        if field not in data:
+            miss_field.append(field)
+    if miss_field :
+        print(f'Missing field: {miss_field}')
+
+    return miss_field
+
+# routing路徑為/log 用HTTP的post
+@app.route('/log', methods=['POST'])
+def log():
+    # data為client打來的JSON格式資料
+    # {"HOST_NAME":"XXXX" , "HOST_IP":"XXXX",.....}
+    data = request.get_json()
+    #檢查是否資料缺失
+    missing_field = check_miss(data)
+    if missing_field:
+        return jsonify({'status': 'error', 'message': f'collector missing field {missing_field}'}), 400
+
+    #檢查數據是否合法
+    data_illegal = check_legal_data(data)
+    if data_illegal:
+        return jsonify({'status': 'error', 'message': f'illegal data  {data_unlegal}'}), 402
+
+    #無資料殘缺
+    try:
+        connection = create_connection()
+        if connection:
+            #創一個游標，用來輸入sql指令
+            cursor = connection.cursor()
+
+            #%s是實際要插入的值,參數化input
+            input_order = """
+            INSERT INTO log_data (HOST_NAME, HOST_IP, SYSTEM_TYPE, LEVEL, PROCESS_NAME, CONTENT, LOG_TIME)
+            VALUES (%s, %s, %s, %s, %s, %s, %s)
+            """
+
+            #執行
+            #data是request進來的JSON，{"HOST_NAME":"XX","HOST_IP":"XX"}
+            cursor.execute(input_order, (
+                data['HOST_NAME'], data['HOST_IP'], data['SYSTEM_TYPE'], data['LEVEL'],
+                data['PROCESS_NAME'], data['CONTENT'], data['LOG_TIME']
+            ))
+
+            #把cursor做的事提交
+            #在執行 INSERT、UPDATE 或 DELETE 操作後，需要調用 commit() 方法來提交這些變更。
+            #如果不調用 commit()，這些變更將不會被保存到資料庫中，並且在連接關閉時將被回滾。
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return jsonify({'status': 'success', 'message': 'Log entry added successfully'}), 201
+
+        #連接失敗，告訴client
+        #原因: (1)sql 帳號密碼錯誤 (2)sql service 沒開
+        else:
+            return jsonify({'status': 'error', 'message': 'Database connection failed'}), 500
+   #非資料庫連接錯誤:logger 的 (1) sql 指令錯誤 (2) python code 寫錯了
+    except Error as e:
+        return jsonify({'status': 'error', 'message': str(e)}), 500
+
 
 if __name__ == '__main__':
     #生產環境要把debug拿掉，host 0000表示接受所有的ip
